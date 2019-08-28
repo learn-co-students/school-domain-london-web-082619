fb5989b470d250b821a0946745a5ebc477d375fc
github username <github email address>
2019-08-28 16:25:12 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+school.add_student("Zach Morris", 9)
+school.roster 
+
+hash ["new_key"] = []
+hash ["new_key"] << "new_value_for_value_array"
+
+hash
+ => {"new_key" => ["new_value_for_vlaue_array"]}
+
+ school.add_student("AC Slater",  9)
+ school.add_student ("Kelly Kapowski, 10")
+ school.add_student ("Screech", 11)
+ school.roster
+
+ school.grade(9)
+
+ school.sort
