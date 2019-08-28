f06f35cc00483f2ce46b1e6328d22c84324fce6e
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-08-28 11:13:56 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    attr_reader :roster
+
+    def add_student(student_name, grade)
+        @roster[grade] = [] if !@roster[grade]
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+end
