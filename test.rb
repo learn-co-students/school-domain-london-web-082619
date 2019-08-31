46d57db0fa28942eb139056c2507323a5728eccf
github username <github email address>
2019-08-31 12:37:06 +0100

    Done.

--- test.rb
+++ test.rb
@@ -1,24 +1,41 @@
+class School
 
+  attr_accessor :name 
+  attr_reader :roster
 
-class Dog
+  def initialize(name)
+      @name = name
+      @roster = {}
+  end
+
+  def add_student(student_name, grade)
+     roster[grade] ||= []
+     roster[grade] << student_name
   
-  def initialize(breed)
-    @breed = breed
+  def grade(grade)
+      roster[grade]
   end
- 
-  def breed=(breed)
-    @breed = breed
+
+  def sort
+    sorted_roster ={}
+    roster.each do |grade, students|
+      sorted_roster[grade] = students.sort
+    end
+    sorted_roster
   end
- 
-  def breed
-    @breed
-  end
+
 end
-  
 
-takeshi = Dog.new("takeshi")
-p takeshi.breed
-takeshi.breed = "takeshi3"
-p takeshi.breed
-takeshi.breed = "takeshi2"
-p takeshi.breed
+end  
+
+tak_school = School.new("Takeshi's")
+tak_school.add_student("student1", 4)
+tak_school.add_student("student2", 4)
+tak_school.add_student("student3", 4)
+tak_school.add_student("student4", 3)
+
+p tak_school.roster
+tak_school.add_student("student5", 5)
+p tak_school.roster
+tak_school.roster = {}
+p tak_school.roster
